820a37ec1404ddce281d19b41b72484bf0cd6c39
kevinalvian <55798459+kevinalvian@users.noreply.github.com>
2019-09-26 08:25:16 +0700

    Add files via upload

---
+++ src/pboif2/pkg10118053/latihan13/relational/PBOIF210118053Latihan13Relational.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118053.latihan13.relational;
+
+/**
+ *
+ * @author 
+ * NAMA : Kevin Alvian
+ * KELAS : IF2
+ * NIM : 10118053
+ */
+public class PBOIF210118053Latihan13Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int bil1 = 10;
+        int bil2 = 20;
+        System.out.println("a == b = " + (bil1 == bil2));
+        System.out.println("a != b = " + (bil1 != bil2));
+        System.out.println("a > b = " + (bil1 > bil2));
+        System.out.println("a < b = " + (bil1 < bil2));
+        System.out.println("b >= a = " + (bil2 >= bil1));
+        System.out.println("b <= a = " + (bil2 <= bil1));
+
+    }
+    
+}
